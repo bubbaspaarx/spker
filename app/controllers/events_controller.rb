427e1baff8d944399f6326d2fdc6987e5794873e
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-05 17:59:09 +0000

    Events can now be created and have a very basic show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_event, except: [:index, :new]
+  before_action :set_user, only: [:new, :create]
+  before_action :authorize_event, except: [:index, :new, :create]
 
   def index
     @events = Event.all
@@ -11,13 +12,20 @@ class EventsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
     @event = Event.new
-    authorize @event
+    authorize_event
 
   end
 
   def create
+    @event = Event.new(event_params)
+    authorize_event
+    @event.user = @user
+    if @event.save!
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -35,8 +43,15 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
   def authorize_event
     authorize @event
   end
 
+  def event_params
+    params.require(:event).permit(:name, :address, :postcode, :date, :start_time, :end_time)
+  end
 end
