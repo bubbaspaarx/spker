6b4954d4136093f066f618bb220db69f161036de
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-05 14:09:35 +0000

    added dependencies to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :user_bookings, dependent: :destroy
+  has_many :speaker_bookings, dependent: :destroy
+
   geocoded_by :postcode
   after_validation :geocode, if: :will_save_change_to_postcode?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :events
+  has_many :events, dependent: :destroy
+  has_many :user_bookings, dependent: :destroy
+  has_many :speaker_bookings, dependent: :destroy
 
   geocoded_by :postcode
   after_validation :geocode, if: :will_save_change_to_postcode?
