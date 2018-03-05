029db970c1ae463f947f175862a63626b40c63ff
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-05 17:38:35 +0000

    implemented flatpickr

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,20 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_event, except: [:index, :new]
 
   def index
+    @events = Event.all
+    authorize @events
   end
 
   def show
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @event = Event.new
+    authorize @event
+
   end
 
   def create
@@ -19,6 +27,16 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def authorize_event
+    authorize @event
   end
 
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -6,7 +6,7 @@ class UserPolicy < ApplicationPolicy
   end
 
   def dashboard?
-    @user == user
+    record == user
   end
 
   def index?
