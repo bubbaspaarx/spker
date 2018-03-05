791c8c825eff5a1b4a734330194c3d3153ab242f
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-05 14:04:13 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :events, only: [:new, :create]
+  end
+  resources :events, only: [:index, :show, :edit, :update, :destroy]
 end
