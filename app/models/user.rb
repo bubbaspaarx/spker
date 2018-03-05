95d6a3227d9feda2d9246b3c1e7daa03c10afeba
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-05 12:43:42 +0000

    user instances should geocode when saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :messages
   has_many :events
+
+  geocoded_by :postcode
+  after_validation :geocode, if: :will_save_change_to_postcode?
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
