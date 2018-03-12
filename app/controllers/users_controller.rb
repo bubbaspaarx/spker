ce6ecb9e811980d0eac714fbd801b3feb95d9c30
Ben Wright <ben1991wright@hotmail.co.uk>
2018-03-12 18:27:59 +0000

    Inbox moved to dashboard and styled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,10 @@ class UsersController < ApplicationController
 
 
   def dashboard
+    @correspondents = assign_correspondents(@user)
   end
 
   def inbox
-    sent = @user.sent_messages
-    sent = sent.map { |message| [message.receiver, message.created_at] }
-    received = @user.received_messages
-    received = received.map { |message| [message.sender, message.created_at] }
-    @correspondents = sent + received
-    @correspondents.sort_by! { |array| array[1] }
-    @correspondents.map! { |array| array[0] }
-    @correspondents.reverse!.uniq!
   end
 
   def conversation
@@ -24,10 +17,6 @@ class UsersController < ApplicationController
     @correspondent = User.find(params[:id])
     @messages = policy_scope(Message)
     @messages = @messages.where('sender_id = ? OR receiver_id = ?', params[:id], params[:id]).order(:created_at)
-    # respond_to do |format|
-    #   format.html
-    #   format.js
-    # end
   end
 
   def index
@@ -52,6 +41,34 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def assign_correspondents(user)
+    sent = user.sent_messages
+    sent = sent.map { |message| [message.receiver, message.created_at] }
+    received = user.received_messages
+    received = received.map { |message| [message.sender, message.created_at] }
+    correspondents = sent + received
+    correspondents.sort_by! { |array| array[1] }
+    correspondents.map! { |array| array[0] }
+    correspondents.reverse!.uniq!
+    correspondents.map do |correspondent|
+      last_message = find_last_message(user, correspondent)
+      [correspondent, last_message]
+    end
+  end
+
+  def find_last_message(user, correspondent)
+    sent = user.sent_messages.where(receiver: correspondent).last
+    received = user.received_messages.where(sender: correspondent).last
+    if sent.nil?
+      received
+    elsif received.nil?
+      sent
+    else
+      sent.created_at > received.created_at ? sent : received
+    end
+
+  end
 
   def set_user
     @user = User.find(params[:id])
