5d26098d6a6418668a8e6bd2afcf10d692d69657
Edward Sparks <edd_sparks@hotmail.com>
2018-03-14 16:15:36 +0000

    made jquery changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import swal from 'sweetalert';
 import '../plugins/flatpickr'
 import 'bootstrap'
+import '../components/login-form.js'
 import { formAlerts, dynamicPrice } from "../components/booking_form.js";
 // import '../components/modal.js'
 import { autocomplete } from '../components/autocomplete.js';
