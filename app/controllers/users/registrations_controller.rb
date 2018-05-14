01c68c1413f72f8d4a28ab9dda7b99baea173ec9
Edward Sparks <edd_sparks@hotmail.com>
2018-05-14 23:11:51 +0100

    added a conditional for an odd registration situation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,7 +18,9 @@ class Users::RegistrationsController < Devise::SessionsController
       clean_up_passwords resource
       set_minimum_password_length
       redirect_to new_user_registration_path(active_tab: 'signup')
-      if resource.email == User.find_by_email(resource.email).email
+      if resource.email? == false
+        set_flash_message :notice, :error
+      elsif resource.email == User.find_by_email(resource.email).email
         set_flash_message :notice, :already
       else
         set_flash_message :notice, :error
