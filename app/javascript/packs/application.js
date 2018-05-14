717e7b713dc36e6024b1d99c7f54a2f06ea47713
Edward Sparks <edd_sparks@hotmail.com>
2018-05-14 17:43:36 +0100

    smooth scroll and login alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import swal from 'sweetalert';
 import '../plugins/flatpickr'
 import 'bootstrap'
 import '../components/login-form.js'
+import '../components/scroll';
 // import { formAlerts, dynamicPrice } from "../components/booking_form.js";
 // import '../components/modal.js'
 import { autocomplete } from '../components/autocomplete.js';
