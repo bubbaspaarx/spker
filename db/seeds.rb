4bd1cd393d8594573c24e33963ac4868be571a35
Edward Sparks <edd_sparks@hotmail.com>
2018-07-11 22:52:56 +0100

    some changes to the topics and talk types

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ Talk.destroy_all
 
 postcodes = ['YO23 3RN', 'HD1 4SH', 'EX11 1LF', 'NE49 9JR', 'TA12 6BP', 'NP8 1DJ', 'PE11 2QR', 'OL2 8QW', 'PE31 7LD', 'NN9 6UT', 'W8 5AR', 'TD9 0BA', 'TS14 6QW', 'BA11 2DW', 'L70 1ET', 'DE65 6PR', 'DY6 9LU', 'NE83 7ZU', 'PO11 0ED', 'SS15 6BZ', 'BS25 1QZ', 'PO8 9BP', 'ST10 1YD', 'N5 2EN', 'DN5 0NB', 'LE9 4BY', 'DG4 6BD', 'WS13 6JB', 'NW9 9TT']
 bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio nisi dolor aut vitae voluptates error iste magni placeat, id non maxime tenetur, fugiat dolorum velit consequatur consectetur qui, culpa laudantium.'
-talk_types = ['Panel Experts and Moderators', 'Keynote Speaker', 'Motivational Speaker', 'Workshop Facilitator', 'After Dinner Speaker', 'Entertainer', 'Comedian', 'MC & Conference Facilitator', 'Award Hosts & Presenters', 'Personal Appearances']
-category_topics = ['Adventure', 'Adversity', 'Arts & Culture', 'Business', 'Change', 'Customer Service', 'Design & Manufacture', 'Diversity & Equality', 'Education', 'Entertainment', 'Entrepreneurs',
-  'Finance & Economics', 'Food & Drink', 'Futurists', 'Health & Wellbeing', 'HR & People Management', 'Human Rights & Philanthropy', 'Innovation', 'International Affairs',
+talk_types = ['Panel Expert/Moderator', 'Keynote Speaker', 'Motivational Speaker', 'Workshop Facilitator', 'After Dinner Speaker', 'Entertainer', 'Comedian', 'MC & Conference Facilitator', 'Award Host & Presenter', 'Personal Appearance']
+category_topics = ['Adventure', 'Adversity', 'Arts & Culture', 'Business', 'Change', 'Customer Service', 'Design & Manufacture', 'Diversity & Equality', 'Education', 'Entertainment', 'Entrepreneurship',
+  'Finance & Economics', 'Food & Drink', 'Futurist', 'Health & Wellbeing', 'HR & People Management', 'Human Rights & Philanthropy', 'Innovation', 'International Affairs',
   'Leadership', 'Marketing & Branding', 'Politics', 'Property', 'Retail', 'Sales', 'Science', 'Security & Risk', 'Social Media', 'Sport', 'Sustainability & Environment', 'Teamwork
-  Technology', 'Transport & Travel', 'TV Presenters']
+  Technology', 'Transport & Travel', 'TV Presenter']
 
   category_topics.each do |topic|
     Category.create(name: topic)
